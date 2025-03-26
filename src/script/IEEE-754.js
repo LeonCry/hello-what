16a8a364c7d6d604bcb7a0aa6eedfa50d75b8b90
icesea <icesea@iceseadeMac-mini.local>
2025-03-26 23:22:25 +0800

    feat: :sparkles: jS 浮点数精度问题

---
+++ src/script/IEEE-754.js
@@ -0,0 +1,8 @@
+const a1 = 0.1;
+const a2 = 0.2;
+const sumA = a1 + a2;
+console.log(sumA); // 0.30000000000000004
+const b1 = 0.5; // 1/2
+const b2 = 0.25; // 1/4
+const sumB = b1 + b2;
+console.log(sumB); // 0.75
